01b88d949aa018d3188536b3a268b9a51add49df
Idan <idan.lizard@gmail.com>
2021-11-12 16:48:39 +0200

    remove unused function

--- snake/direction.go
+++ snake/direction.go
@@ -2,7 +2,6 @@ package snake
 
 import (
 	"snake/board"
-	"strings"
 )
 
 type Direction string
@@ -12,21 +11,10 @@ const (
 	Down  Direction = "down"
 	Left  Direction = "left"
 	Right Direction = "right"
-	None  Direction = "NONE"
 )
 
 func (this Direction) String() string {
 	return string(this)
-}
-
-func DirectionFromString(s string) Direction {
-	d := Direction(strings.ToLower(s))
-	switch d {
-	case Up, Down, Left, Right:
-		return d
-	default:
-		return None
-	}
 }
 
 func (this Direction) TranslatePoint(p board.Point) board.Point {
@@ -48,8 +36,4 @@ func (this Direction) TranslatePoint(p board.Point) board.Point {
 
 func (this Direction) IsParallelTo(d Direction) bool {
 	return (this == Up || this == Down) == (d == Up || d == Down)
-}
-
-func (this Direction) IsOppositeTo(d Direction) bool {
-	return this != d && this.IsParallelTo(d)
 }
